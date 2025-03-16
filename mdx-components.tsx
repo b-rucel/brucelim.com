c5b2fabe57c2647dbe09315b327e4e61f965c212
b-rucel <b-rucel@users.noreply.github.com>
2025-03-16 11:47:58 -0700

    adding styles to the blog pages

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,7 +1,9 @@
 import type { MDXComponents } from 'mdx/types';
+import './app/styles/mdx.css';
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    wrapper: ({ children }) => <div className="mdx-content">{children}</div>,
     ...components,
   };
 }
