a8c7962c013d03cf748d88c4012f4b3926ee7719
b-rucel <b-rucel@users.noreply.github.com>
2025-04-07 10:39:08 -0700

    updating ga filtering to client

--- components/GoogleAnalytics.tsx
+++ components/GoogleAnalytics.tsx
@@ -1,30 +1,34 @@
 'use client';
 
-export default function GoogleAnalytics({ measurementId }: { measurementId: string }) {
-  if (typeof window === 'undefined' || (
-    window.location.hostname === 'localhost' ||
-    window.location.hostname.endsWith('.pages.dev')
-  )) {
-    console.log('Google Analytics is disabled for development.')
-    return null;
-  }
+import { useEffect } from 'react';
 
-  return (
-    <>
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-      />
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${measurementId}');
-          `,
-        }}
-      />
-    </>
-  );
+export default function GoogleAnalytics({ measurementId }: { measurementId: string }) {
+  useEffect(() => {
+    if (
+      typeof window === 'undefined' ||
+      window.location.hostname === 'localhost' ||
+      window.location.hostname.endsWith('.pages.dev')
+    ) {
+      console.log('Google Analytics is disabled for development.');
+      return;
+    }
+
+    // Load the Google Analytics script
+    const script = document.createElement('script');
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+    script.async = true;
+
+    script.onload = () => {
+      window.dataLayer = window.dataLayer || [];
+      function gtag(...args: any[]) {
+        window.dataLayer.push(arguments);
+      }
+      gtag('js', new Date());
+      gtag('config', measurementId);
+    };
+
+    document.head.appendChild(script);
+  }, [measurementId]);
+
+  return null;
 }
